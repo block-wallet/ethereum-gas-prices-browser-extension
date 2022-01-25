114a5c027bc68e6bbf530d90d5bcccf953bd07a8
Philippe <philippe-git@protonmail.com>
2022-01-25 13:47:01 +0100

    Use cdn for blocknative api

--- src/utils.js
+++ src/utils.js
@@ -27,23 +27,9 @@ const debounce = (fn) => {
   });
 };
 
-const blocknativeApiFetchOptions = {
-  method: 'GET',
-  headers: { Authorization: '7ef1ad25-e1f0-41f6-b01b-d39615186be8' },
-};
-
-const getBlocknativeData = memoizeAsync(async () => {
-  const resp = await (await fetch('https://api.blocknative.com/gasprices/blockprices?confidenceLevels=99&confidenceLevels=90&confidenceLevels=80&confidenceLevels=60', blocknativeApiFetchOptions)).json();
-  const { blockPrices: [{ baseFeePerGas, estimatedPrices }] } = resp;
-
-  return {
-    baseFee: baseFeePerGas,
-    fastest: estimatedPrices.find(({ confidence }) => confidence === 99),
-    fast: estimatedPrices.find(({ confidence }) => confidence === 90),
-    standard: estimatedPrices.find(({ confidence }) => confidence === 80),
-    slow: estimatedPrices.find(({ confidence }) => confidence === 60),
-  };
-});
+const getBlocknativeData = memoizeAsync(async () => (
+  (await fetch('https://opfi.fr/api/blocknative')).json()
+));
 
 const getEtherscanData = memoizeAsync(async () => (
   (await fetch('https://api.etherscan.io/api?module=gastracker&action=gasoracle')).json()
